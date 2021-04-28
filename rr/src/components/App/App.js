49f60622fba170a32a19e739ba9f16074c0c2c45
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-28 14:13:06 +0200

    Adding the Switch component inside BrowserRouter

--- rr/src/components/App/App.js
+++ rr/src/components/App/App.js
@@ -8,7 +8,13 @@ function App() {
   return (
     <div className="wrapper">
       <h1>Marine Mammals</h1>
+      <BrowserRouter>
+       <Switch>
+          <Route path="/">
       <Manatee />
+       </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
