c191a939c8609441d7efbb1cf294f035a38a18ba
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-28 14:21:19 +0200

    Update the route for the Manatee component to /manatee, then import the remaining components and create a route for each

--- rr/src/components/App/App.js
+++ rr/src/components/App/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Manatee from '../Manatee/Manatee';
+import Narwhal from '../Narwhal/Narwhal';
+import Whale from '../Whale/Whale';
 
 function App() {
   return (
@@ -10,10 +12,16 @@ function App() {
       <h1>Marine Mammals</h1>
       <BrowserRouter>
        <Switch>
-          <Route path="/">
-      <Manatee />
-       </Route>
-        </Switch>
+         <Route path="/manatee">
+            <Manatee />
+          </Route>
+          <Route path="/narwhal">
+            <Narwhal />
+          </Route>
+          <Route path="/whale">
+            <Whale />
+          </Route> 
+       </Switch>
       </BrowserRouter>
     </div>
   );
